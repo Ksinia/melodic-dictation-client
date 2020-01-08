565c6179bb2c6dcb895b50eff2bdcae4bf810ece
Ksinia <xiatian@mail.ru>
2020-01-08 17:18:05 +0100

    Set up for list of melodies

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Switch, Route } from "react-router-dom";
 import LoginContainer from "./components/LoginContainer";
 import SignupContainer from "./components/SignupContainer";
 import { connect } from "react-redux";
+import MelodyListContainer from "./components/MelodyListContainer";
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <Switch>
           <Route path="/signup" component={SignupContainer} />
           <Route path="/login" component={LoginContainer} />
+          <Route path="/" component={MelodyListContainer} />
         </Switch>
       </div>
     );
---
+++ src/components/MelodyListContainer.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import MelodyList from "./MelodyList";
+
+class MelodyListContainer extends Component {
+  render() {
+    return <MelodyList />;
+  }
+}
+
+export default MelodyListContainer;

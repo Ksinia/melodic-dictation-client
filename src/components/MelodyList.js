cab351e5f0ff308cd24a81caf42bcb4e31881dea
Ksinia <xiatian@mail.ru>
2020-01-09 15:45:49 +0100

    Add links to melodies in the list, add play button on the details page

--- src/components/MelodyList.js
+++ src/components/MelodyList.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function MelodyList(props) {
-  return props.melodies
-    ? props.melodies.map(melody => <p>{melody.name}</p>)
-    : "Loading...";
+  return (
+    <div>
+      <h1>List of melodies</h1>
+      {props.melodies
+        ? props.melodies.map(melody => (
+            <Link key={melody.id} to={`/melody/${melody.id}`}>
+              <p>{melody.name}</p>
+            </Link>
+          ))
+        : "Loading..."}
+    </div>
+  );
 }
 
 export default MelodyList;

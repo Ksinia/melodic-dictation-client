10a4ba5ffefdceb3018141de47469a3810ae3417
Ksinia <xiatian@mail.ru>
2020-01-09 15:45:49 +0100

    Add links to melodies in the list, add play button on the details page

--- src/components/MelodyDetailsPageContainer.js
+++ src/components/MelodyDetailsPageContainer.js
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
 import { loadMelody, clearMelodyDetails } from "../actions/melody";
 import { connect } from "react-redux";
+// import * as fs from "fs-web";
+var fs = require("browserify-fs");
+// import MidiPlayer from "midi-player-js";
+// import { JZZ } from "jzz-midi-smf";
+const { Midi } = require("@tonejs/midi");
+var JZZ = require("jzz");
+require("jzz-midi-smf")(JZZ);
 
 class MelodyDetailsPageContainer extends Component {
   melodyId = this.props.match.params.melodyId;
+  play = () => {
+    var midiout = JZZ().openMidiOut();
+    var smf = new JZZ.MIDI.SMF(this.props.melody.content);
+    var player = smf.player();
+    player.connect(midiout);
+    player.play();
+  };
+
   componentDidMount() {
     this.props.dispatch(clearMelodyDetails());
     this.props.dispatch(loadMelody(this.melodyId));
@@ -17,6 +32,7 @@ class MelodyDetailsPageContainer extends Component {
           <div>
             <p>{this.props.melody.name}</p>
             <p>{this.props.melody.url}</p>
+            <button onClick={this.play}>Play</button>
           </div>
         ) : (
           "Loading..."

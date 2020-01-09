ec32358e861def97eb321795705db3256f1f0964
Ksinia <xiatian@mail.ru>
2020-01-09 23:09:43 +0100

    First working version of midi player

--- src/components/MelodyDetailsPageContainer.js
+++ src/components/MelodyDetailsPageContainer.js
@@ -1,45 +1,16 @@
 import React, { Component } from "react";
 import { loadMelody, clearMelodyDetails } from "../actions/melody";
 import { connect } from "react-redux";
-import { Midi } from "@tonejs/midi";
-import Tone from "tone";
-
-/*
-var synth = new Tone.PolySynth(8).toMaster()
-MidiConvert.load("path/to/midi.mid", function(midi) {
-  // make sure you set the tempo before you schedule the events
-  Tone.Transport.bpm.value = midi.header.bpm
-  // pass in the note events from one of the tracks as the second argument to Tone.Part 
-  var midiPart = new Tone.Part(function(time, note) {
-    //use the events to play the synth
-    synth.triggerAttackRelease(note.name, note.duration, time, note.velocity)
-  }, midi.tracks[0].notes).start()
-  // start the transport to hear the events
-  Tone.Transport.start()
-})
-*/
+import MidiPlayer from "web-midi-player";
 
 class MelodyDetailsPageContainer extends Component {
   melodyId = this.props.match.params.melodyId;
-  play = () => {
-    console.log("not breaking");
-    const midiFile = new Midi(Buffer.from(this.props.melody.content, "binary"));
-    console.log("MIDI: ", midiFile);
+  midiPlayer = new MidiPlayer();
 
-    var synth = new Tone.PolySynth(8).toMaster();
-    Tone.Transport.bpm.value = midiFile.header.tempos[0].bpm;
-    // pass in the note events from one of the tracks as the second argument to Tone.Part
-    const myMidi = new Tone.Part(function(time, note) {
-      //use the events to play the synth
-      synth.triggerAttackRelease(note.name, note.duration, time, note.velocity);
-    }, midiFile.tracks[0].notes);
-    myMidi.start();
-    /*
-    const player = new Tone.Player(
-      Buffer.from(this.props.melody.content, "binary")
-    );
-    synth.triggerAttack("D3", "+1");
-    Tone.start();*/
+  play = () => {
+    this.midiPlayer.stop();
+    const buffer = Buffer.from(this.props.melody.content, "binary");
+    this.midiPlayer.play({ arrayBuffer: buffer });
   };
 
   componentDidMount() {

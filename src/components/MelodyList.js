331be1c5e91b51376fb91628e8a0290afab2fd0b
Ksinia <xiatian@mail.ru>
2020-01-16 12:35:38 +0100

    Stylize melody list, add play button to melodies in list, add popularity to melodies

--- src/components/MelodyList.js
+++ src/components/MelodyList.js
@@ -1,19 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./MelodyList.css";
 
 function MelodyList(props) {
-  return (
-    <div>
-      <h1>List of melodies</h1>
+  return [
+    <h1 key="title">List of melodies</h1>,
+    <div key="list" className="melody-list">
       {props.melodies
         ? props.melodies.map(melody => (
-            <Link key={melody.id} to={`/melody/${melody.id}`}>
-              <p>{melody.name}</p>
-            </Link>
+            <div key={melody.id} className="list-element">
+              <div className="text">
+                <Link to={`/melody/${melody.id}`}>
+                  <p>{melody.name}</p>
+                </Link>
+                <p>Popularity: {melody.dictationsCount}</p>
+              </div>
+              <button
+                className="play-button-in-list"
+                onClick={() => props.play(melody.url)}
+              >
+                Play
+              </button>
+            </div>
           ))
         : "Loading..."}
     </div>
-  );
+  ];
 }
 
 export default MelodyList;

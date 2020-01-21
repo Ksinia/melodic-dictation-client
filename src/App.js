4ea704679fab5740e876fda8fed4282d43667f77
Ksinia <xiatian@mail.ru>
2020-01-21 22:31:01 +0100

    Add github corner

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-// import logo from "./logo.svg";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import LoginContainer from "./components/LoginContainer";

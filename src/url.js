b98faa887eb03b615efe584802ed09b720948c06
Ksinia <xiatian@mail.ru>
2020-05-05 15:25:43 +0200

    Fix backend url

--- src/url.js
+++ src/url.js
@@ -1,2 +1,5 @@
-// export const url = "http://localhost:4000";
-export const url = "https://melodic-dictation.herokuapp.com";
+let url = "https://melodic-dictation.herokuapp.com";
+if (process.env.NODE_ENV === "development") {
+  url = process.env.REACT_APP_API_URL || "http://localhost:4000";
+}
+export { url };
